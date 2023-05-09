e5eba5351eca88558083868f1cd47b1d953bc71e
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-05-08 21:53:09 -0300

    ok

--- client/src/components/portfolio/portfolioGrid.js
+++ client/src/components/portfolio/portfolioGrid.js
@@ -4,21 +4,17 @@ export default function PortfolioGrid({ items }) {
   return (
     <div>
       <Quick />
-      <AnimatePresence>
-        <motion.div
-          className=" grid h-auto  w-full grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-4 "
-          //animacion desde la derecha
-          initial={{ x: "100%" }}
-          animate={{ x: 0 }}
-          transition={{ duration: 0.6 }}
-        >
-          {items?.map((item) => (
-            <div key={item.id}>
-              <CardGrid item={item} />
-            </div>
-          ))}
-        </motion.div>
-      </AnimatePresence>
+
+      <div
+        className=" grid h-auto w-full max-w-[100vh] grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-4 "
+        //animacion desde la derecha
+      >
+        {items?.map((item) => (
+          <div key={item.id}>
+            <CardGrid item={item} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
