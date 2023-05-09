776bb531a8bd325bcc9d26fb76477e787067844e
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-05-08 22:07:51 -0300

    bug grid

--- client/src/components/portfolio/portfolioGrid.js
+++ client/src/components/portfolio/portfolioGrid.js
@@ -2,11 +2,10 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Quick, quickVisibilityExternal, CardGrid } from "@/components";
 export default function PortfolioGrid({ items }) {
   return (
-    <div>
+    <>
       <Quick />
-
       <div
-        className=" grid h-auto w-full max-w-[100vh] grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-4 "
+        className=" grid h-auto min-w-full max-w-[100vh] grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-4 "
         //animacion desde la derecha
       >
         {items?.map((item) => (
@@ -15,6 +14,6 @@ export default function PortfolioGrid({ items }) {
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
 }
