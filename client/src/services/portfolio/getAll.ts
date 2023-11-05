e0036037e25d6c821bca8ec6f4c5eb3b797549f2
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-11-05 01:38:48 -0300

    chore: cache config

--- client/src/services/portfolio/getAll.ts
+++ client/src/services/portfolio/getAll.ts
@@ -9,13 +9,7 @@ interface Response {
 }
 
 export const getProjects = async (): Promise<Response> => {
-  const { data, error } = await getRequest(
-    Endpoints.PROJECTS,
-    {},
-    {
-      cache: 'no-cache'
-    }
-  )
+  const { data, error } = await getRequest(Endpoints.PROJECTS, {})
   if (error) throw new Error('Error al obtener los proyectos')
   console.log('data', data)
   return { data: formatProjectUrl(data.result), error }
